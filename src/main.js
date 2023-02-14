d93935a39d10948d969446840abe3db49260e0f3
laura romero <lauraromerocx@gmail.com>
2023-02-14 12:18:30 -0500

    cambios sin contenido, se borro

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
-import { welcome } from './components/welcome.js'
-// variable que accede al nodo que se creo en index, llamado "root" //
-const root = document.getElementById ('root');
-const routes = {
-'/': welcome
-};
-
-root.appendChild(welcome());
 
 
